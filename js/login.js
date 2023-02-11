f2e8f9ee0e7dbd2310fd24d74d4e31fc3688db02
ali1924 <mohammadali229987@gmail.com>
2023-02-12 00:10:52 +0600

    Deposit, withdraw, Balance grid bank html file

--- js/login.js
+++ js/login.js
@@ -18,8 +18,10 @@ document.getElementById('btn-submit').addEventListener('click',function(){
     // step-4 varify email and password
     if(email==='sontan@baap.com' && password==='secret'){
         console.log('Valid User');
+        window.location.href='bank.html';
     }
     else{
         console.log('Invalid User');
+        window.location.href='bank.html';
     }
 })
